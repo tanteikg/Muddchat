76cd1e8c60f532f2d168d7859c06e804accb3532
tanteikg <tanteikg@gmail.com>
2016-05-02 09:03:26 +0800

    Added files via upload

---
+++ aiml.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Botkit = require('botkit')
+var aiml = require('aimlinterpreter')
+
+var slackToken = process.env.SLACK_TOKEN
+var controller = Botkit.slackbot({ debug: false })
+
+controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) 
+{
+  if (err) 
+    throw new Error('Error connecting to Slack: ', err)
+  console.log('Connected to Slack')
+});
+
+var aimlInterpreter = new aiml({name:'testbot', age:'1'});
+
+aimlInterpreter.loadAIMLFilesIntoArray(['./knockknock.aiml']);
+var callback = function(answer, wildCardArray, input){
+console.log(answer + ' | ' + wildCardArray + ' | ' + input);
+};
+
+controller.hears('.*',   'direct_message,direct_mention,mention', function (bot,message) 
+{
+  const msg = message.text;
+
+  console.log('received message : ',msg);
+  aimlInterpreter.findAnswerInLoadedAIMLFiles(msg, function(answer, wildCardArray,input)
+  {
+    if (answer == undefined)
+    {
+      console.log('reply is undefined');
+      bot.reply(message,'I have no idea what you are saying');
+    }
+    else
+    {
+      console.log('replying with:', answer, '| wildcard :',wildCardArray,' | input: ',input);
+
+      bot.reply(message, answer);
+    }
+  });
+
+});
+
